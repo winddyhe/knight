cdbf1c2639ca9b88e6ade7369b6c74e27aa78ba3
winddyhe <hgplan@126.com>
2017-05-26 00:33:57 +0800

    【资源管理模块】，自定义Assetbundle版本文件。

---
+++ Assets/Editor/Assetbundles/AssetVersion.Editor.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.AssetBundles;
+using Core;
+using System.IO;
+
+namespace UnityEditor.AssetBundles
+{
+    public static class AssetVersionEditor
+    {
+        public static void Save(this AssetVersion rVersion, string rOutPath)
+        {
+            if (rVersion == null) return;
+            if (File.Exists(rOutPath)) File.Delete(rOutPath);
+
+            AssetDatabase.CreateAsset(rVersion, rOutPath);
+            AssetDatabase.Refresh();
+            AssetDatabase.SaveAssets();
+        }
+
+        public static AssetVersion CreateVersion(string rOutPath, AssetVersion rOldVersion, AssetBundleManifest rNewABManifest)
+        {
+            AssetVersion rVersion = ScriptableObject.CreateInstance<AssetVersion>();
+            rVersion.Entries = new Dict<string, AssetVersionEntry>();
+
+            string[] rAllAssetbundles = rNewABManifest.GetAllAssetBundles();
+            for (int i = 0; i < rAllAssetbundles.Length; i++)
+            {
+                AssetVersionEntry rAVEntry = new AssetVersionEntry();
+                rAVEntry.Name = rAllAssetbundles[i];
+
+                var rOldEntry = rOldVersion.GetEntry(rAllAssetbundles[i]);
+                string rOldMD5 = rOldEntry != null ? rOldEntry.MD5 : string.Empty;
+                string rNewMD5 = GetMD5InManifest(rNewABManifest, rAllAssetbundles[i]);
+
+                rAVEntry.MD5 = rNewMD5;
+                if (!string.IsNullOrEmpty(rOldMD5) && rOldMD5.Equals(rNewMD5))
+                {
+                    rAVEntry.Version = rAVEntry.Version + 1;
+                }
+                rAVEntry.Size = GetABSizeInManifest(rOutPath, rAllAssetbundles[i]);
+                rAVEntry.Dependencies = rNewABManifest.GetDirectDependencies(rAllAssetbundles[i]);
+                rVersion.Entries.Add(rAllAssetbundles[i], rAVEntry);
+            }
+            return rVersion;
+        }
+
+        public static string GetMD5InManifest(AssetBundleManifest rManifest, string rABName)
+        {
+            if (rManifest == null) return string.Empty;
+            return rManifest.GetAssetBundleHash(rABName).ToString();
+        }
+
+        public static long GetABSizeInManifest(string rOutPath, string rABName)
+        {
+            string rABFilePath = rOutPath + "/" + rABName;
+            var rABFileInfo = new FileInfo(rABFilePath);
+            if (rABFilePath != null)
+            {
+                return rABFilePath.Length;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Assets/Framework/Assetbundles/AssetVersion.cs
@@ -0,0 +1,37 @@
+﻿//======================================================================
+//        Copyright (C) 2015-2020 Winddy He. All rights reserved
+//        Email: hgplan@126.com
+//======================================================================
+using Core;
+
+namespace UnityEngine.AssetBundles
+{
+    [System.Serializable]
+    public class AssetVersionEntry
+    {
+        public string   N;
+        public int      V;
+        public string   M;
+        public long     S;
+        public string[] D;
+        
+        public string   Name         { get { return N; } set { N = value; } }
+        public int      Version      { get { return V; } set { V = value; } }
+        public string   MD5          { get { return M; } set { M = value; } }
+        public long     Size         { get { return S; } set { S = value; } }
+        public string[] Dependencies { get { return D; } set { D = value; } }
+    }
+
+    public partial class AssetVersion : ScriptableObject
+    {
+        public Dict<string, AssetVersionEntry> Entries;
+
+        public AssetVersionEntry GetEntry(string rABName)
+        {
+            if (this.Entries == null) return null;
+            AssetVersionEntry rEntry = null;
+            this.Entries.TryGetValue(rABName, out rEntry);
+            return rEntry;
+        }
+    }
+}
