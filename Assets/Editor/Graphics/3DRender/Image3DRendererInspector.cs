0877ad3b2afcefb7020504ff4df8faf13af4355a
winddyhe <hgplan@126.com>
2017-07-01 22:11:18 +0800

    Editor目录结构变更。新建Image3DRendererInspector。

---
+++ Assets/Editor/Graphics/3DRender/Image3DRendererInspector.cs
@@ -0,0 +1,18 @@
+﻿//======================================================================
+//        Copyright (C) 2015-2020 Winddy He. All rights reserved
+//        Email: hgplan@126.com
+//======================================================================
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace Framework.Graphics.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Image3DRenderer), true)]
+    public class Image3DRendererInspector : UnityEditor.Editor
+    {
+
+    }
+}
+
