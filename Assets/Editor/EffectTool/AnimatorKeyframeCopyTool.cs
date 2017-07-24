d4288e4f2d2a2511f6e4a5cc009054702891c901
winddyhe <hgplan@126.com>
2017-07-24 23:21:46 +0800

    特效关键帧复制工具初步。

---
+++ Assets/Editor/EffectTool/AnimatorKeyframeCopyTool.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Framework.Editor
+{
+    public class AnimatorKeyframeCopyTool : UnityEditor.Editor
+    {
+        public static void CopyKeyFrames()
+        {
+
+        }
+
+        public static void PasteKeyFrames()
+        {
+
+        }
+    }
+}
