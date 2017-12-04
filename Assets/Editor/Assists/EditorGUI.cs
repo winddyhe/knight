43a1134ebf2cf0536aaa5f65c9e84a799eb60dac
winddyhe <hgplan@126.com>
2017-12-05 00:19:00 +0800

    Editor GUI types module.

---
+++ Assets/Editor/Assists/EditorGUI.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Core.Editor
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class EditorTypeAttribute : Attribute
+    {
+        public Type     Type;
+
+        public EditorTypeAttribute(Type rType)
+        {
+            this.Type = rType;
+        }
+    }
+
+    public class EditorGUITypes : TypeSearchDefault<EditorGUIBase>
+    {
+    }
+
+    [TSIgnore]
+    public class EditorGUIBase
+    {
+        public object   Value;
+
+        public string   HeadText;
+        public float    Width;
+
+        public virtual void OnGUI()
+        {
+        }
+    }
+
+    [EditorType(typeof(float))]
+    public class EditorGUIFloat : EditorGUIBase
+    {
+        public override void OnGUI()
+        {
+            float fValue = (float)this.Value;
+            fValue = EditorGUILayout.FloatField(this.HeadText, fValue, GUILayout.Width(this.Width));
+            this.Value = fValue;
+        }
+    }
+
+    [EditorType(typeof(bool))]
+    public class EditorGUIBool : EditorGUIBase
+    {
+        public override void OnGUI()
+        {
+            bool bValue = (bool)this.Value;
+            bValue = EditorGUILayout.Toggle(this.HeadText, bValue, GUILayout.Width(this.Width));
+            this.Value = bValue;
+        }
+    }
+
+    [EditorType(typeof(Vector2))]
+    public class EditorGUIVector2 : EditorGUIBase
+    {
+        public override void OnGUI()
+        {
+            Vector2 rValue = (Vector2)this.Value;
+            rValue = EditorGUILayout.Vector2Field(this.HeadText, rValue, GUILayout.Width(this.Width));
+            this.Value = rValue;
+        }
+    }
+
+    [EditorType(typeof(Vector3))]
+    public class EditorGUIVector3 : EditorGUIBase
+    {
+        public override void OnGUI()
+        {
+            Vector3 rValue = (Vector3)this.Value;
+            rValue = EditorGUILayout.Vector3Field(this.HeadText, rValue, GUILayout.Width(this.Width));
+            this.Value = rValue;
+        }
+    }
+
+    [EditorType(typeof(Vector4))]
+    public class EditorGUIVector4 : EditorGUIBase
+    {
+        public override void OnGUI()
+        {
+            Vector4 rValue = (Vector4)this.Value;
+            rValue = EditorGUILayout.Vector4Field(this.HeadText, rValue, GUILayout.Width(this.Width));
+            this.Value = rValue;
+        }
+    }
+
+    [EditorType(typeof(string))]
+    public class EditorGUIString : EditorGUIBase
+    {
+        public override void OnGUI()
+        {
+            string rValue = (string)this.Value;
+            rValue = EditorGUILayout.TextField(this.HeadText, rValue, GUILayout.Width(this.Width));
+            this.Value = rValue;
+        }
+    }
+
+    [TSIgnore]
+    public class EditorGUIObject : EditorGUIBase
+    {
+        private List<EditorGUIBase> mGUIControllers;
+
+        public EditorGUIObject(object rObject)
+        {
+            this.Value = rObject;
+            this.Anaysis(this.Value);
+        }
+
+        private void Anaysis(object rValue)
+        {
+            this.mGUIControllers = new List<EditorGUIBase>();
+            Type rObjType = rValue.GetType();
+        }
+
+        public override void OnGUI()
+        {
+
+        }
+    }
+}
---
+++ Assets/Editor/ECSTool/ECSTool.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Framework.ECS.Editor
+{
+    public class ECSTool : UnityEditor.Editor
+    {
+
+    }
+}
