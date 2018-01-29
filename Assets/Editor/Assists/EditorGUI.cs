cdd05a39aba95b5cfeeaf92f4b8dbf51d25ef1e6
winddyhe <hgplan@126.com>
2018-01-29 21:52:45 +0800

    Editor GUI module.

--- Assets/Editor/Assists/EditorGUI.cs
+++ Assets/Editor/Assists/EditorGUI.cs
@@ -5,136 +5,53 @@ using UnityEngine;
 
 namespace Core.Editor
 {
-    [AttributeUsage(AttributeTargets.Class)]
-    public class EditorTypeAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class EditorGUIAttribute : Attribute
     {
-        public Type     Type;
-
-        public EditorTypeAttribute(Type rType)
+        public string       HeadText;
+        public int          Width;
+        
+        public EditorGUIAttribute()
         {
-            this.Type = rType;
+            this.HeadText   = string.Empty;
+            this.Width      = 0;
+        }
+
+        public EditorGUIAttribute(string rHeadText, int nWidth)
+        {
+            this.HeadText   = rHeadText;
+            this.Width      = nWidth;
         }
     }
 
-    public class EditorGUITypes : TypeSearchDefault<EditorGUIBase>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class EditorGUIIgnoreAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class EditorGUIEnableAttribute : Attribute { }
+
+    public class EditorGUIAssists
     {
-    }
-
-    [TSIgnore]
-    public class EditorGUIBase
-    {
-        public object   Value;
-
-        public string   HeadText;
-        public float    Width;
-
-        public virtual void OnGUI()
+        public static void OnGUI(object rValue)
         {
-        }
-    }
 
-    [EditorType(typeof(float))]
-    public class EditorGUIFloat : EditorGUIBase
-    {
-        public override void OnGUI()
-        {
-            float fValue = (float)this.Value;
-            fValue = EditorGUILayout.FloatField(this.HeadText, fValue, GUILayout.Width(this.Width));
-            this.Value = fValue;
-        }
-    }
-
-    [EditorType(typeof(bool))]
-    public class EditorGUIBool : EditorGUIBase
-    {
-        public override void OnGUI()
-        {
-            bool bValue = (bool)this.Value;
-            bValue = EditorGUILayout.Toggle(this.HeadText, bValue, GUILayout.Width(this.Width));
-            this.Value = bValue;
-        }
-    }
-
-    [EditorType(typeof(Vector2))]
-    public class EditorGUIVector2 : EditorGUIBase
-    {
-        public override void OnGUI()
-        {
-            Vector2 rValue = (Vector2)this.Value;
-            rValue = EditorGUILayout.Vector2Field(this.HeadText, rValue, GUILayout.Width(this.Width));
-            this.Value = rValue;
-        }
-    }
-
-    [EditorType(typeof(Vector3))]
-    public class EditorGUIVector3 : EditorGUIBase
-    {
-        public override void OnGUI()
-        {
-            Vector3 rValue = (Vector3)this.Value;
-            rValue = EditorGUILayout.Vector3Field(this.HeadText, rValue, GUILayout.Width(this.Width));
-            this.Value = rValue;
-        }
-    }
-
-    [EditorType(typeof(Vector4))]
-    public class EditorGUIVector4 : EditorGUIBase
-    {
-        public override void OnGUI()
-        {
-            Vector4 rValue = (Vector4)this.Value;
-            rValue = EditorGUILayout.Vector4Field(this.HeadText, rValue, GUILayout.Width(this.Width));
-            this.Value = rValue;
-        }
-    }
-
-    [EditorType(typeof(string))]
-    public class EditorGUIString : EditorGUIBase
-    {
-        public override void OnGUI()
-        {
-            string rValue = (string)this.Value;
-            rValue = EditorGUILayout.TextField(this.HeadText, rValue, GUILayout.Width(this.Width));
-            this.Value = rValue;
-        }
-    }
-
-    [TSIgnore]
-    public class EditorGUIObject : EditorGUIBase
-    {
-        private List<EditorGUIBase> mGUIControllers;
-        private List<Type>          mTypes;
-
-        public EditorGUIObject(object rObject)
-        {
-            this.Value = rObject;
-            this.Anaysis(this.Value);
         }
 
-        private void Anaysis(object rValue)
+        public bool IsGUIType(Type rType)
         {
-            this.mTypes = new List<Type>();
-            for (int i = 0; i < EditorGUITypes.Types.Count; i++)
+            bool bIsGUIType = false;
+            if (rType.IsPrimitive)
             {
-                Type rGUIType = EditorGUITypes.Types[i];
-                var rEditorAttr = rGUIType.GetCustomAttribute<EditorTypeAttribute>(false);
-                if (rEditorAttr != null)
-                    this.mTypes.Add(rEditorAttr.Type);
+                bIsGUIType = true;
             }
-
-            this.mGUIControllers = new List<EditorGUIBase>();
-            Type rObjType = rValue.GetType();
-
-            var rAllFields = rObjType.GetFields(System.Reflection.BindingFlags.CreateInstance);
-            for (int i = 0; i < rAllFields.Length; i++)
+            else
             {
-                Type rType = rAllFields[i].FieldType;
+                if (rType == typeof(Vector2) || rType == typeof(Vector3) || rType == typeof(Vector4) || rType.IsEnum)
+                {
+                    bIsGUIType = true;
+                }
             }
-        }
-
-        public override void OnGUI()
-        {
-
+            return bIsGUIType;
         }
     }
 }
