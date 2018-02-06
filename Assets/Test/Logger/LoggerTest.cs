9e1d45052c15e173c0e57565fb6c7942bc6bb4c0
winddyhe <hgplan@126.com>
2018-02-06 16:16:03 +0800

    Logger module.

---
+++ Assets/Test/Logger/LoggerTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class LoggerTest : MonoBehaviour
+    {
+        void Start()
+        {
+            Core.Logger.Info("xx1xxx");
+            Core.Logger.Info("xx2xxx");
+            Core.Logger.Info("xx3xxx");
+        }
+    }
+}
