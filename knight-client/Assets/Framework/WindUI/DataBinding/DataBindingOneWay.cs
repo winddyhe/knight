0e59d04700840b76afa79676a29f04f4cdecd9a4
winddyhe <hgplan@126.com>
2018-06-28 01:07:21 +0800

    MVVM UI framework first building.

---
+++ knight-client/Assets/Framework/WindUI/DataBinding/DataBindingOneWay.cs
@@ -0,0 +1,24 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.UI
+{
+    public class DataBindingOneWay : MonoBehaviour
+    {
+        [Dropdown("xx")]
+        public string ModelPath;
+        public string ViewPath;
+
+        public string[] xx;
+
+        void Start()
+        {
+        }
+        
+        void Update()
+        {
+        }
+    }
+}
