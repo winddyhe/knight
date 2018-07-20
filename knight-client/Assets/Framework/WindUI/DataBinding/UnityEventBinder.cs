38569ad79067be671f096c4bdb27f97f04da3f11
winddyhe <winddyhe@users.noreply.github.com>
2018-07-20 20:06:01 +0800

    Add unity event binder.

---
+++ knight-client/Assets/Framework/WindUI/DataBinding/UnityEventBinder.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+using Knight.Core;
+
+namespace UnityEngine.UI
+{
+    public static class UnityEventBinderFactory
+    {
+        public static UnityEventBinderBase Create(UnityEventBase rUnityEventBase, Action rAction)
+        {
+            var eventArgumentTypes = rUnityEventBase.GetType().BaseType.GetGenericArguments();
+
+            if (!eventArgumentTypes.Any())
+            {
+                return new UnityEventBinder(rUnityEventBase, rAction);
+            }
+
+            try
+            {
+                var genericType = typeof(UnityEventBinder<>).MakeGenericType(eventArgumentTypes);
+                return (UnityEventBinderBase)Activator.CreateInstance(genericType, rUnityEventBase, rAction);
+            }
+            catch (ArgumentException ex)
+            {
+                throw new Exception("Cannot bind event with more than 5 arguments", ex);
+            }
+        }
+    }
+
+    public abstract class UnityEventBinderBase : IDisposable
+    {
+        public abstract void Dispose();
+    }
+
+    public class UnityEventBinder : UnityEventBinderBase
+    {
+        private UnityEvent      mUnityEvent;
+        private readonly Action mAction;
+
+        public UnityEventBinder(UnityEventBase rUnityEvent, Action rAction)
+        {
+            this.mUnityEvent = (UnityEvent)rUnityEvent;
+            this.mAction     = rAction;
+
+            this.mUnityEvent.AddListener(EventHandler);
+        }
+
+        public override void Dispose()
+        {
+            if (mUnityEvent == null) return;
+
+            mUnityEvent.RemoveListener(EventHandler);
+            mUnityEvent = null;
+        }
+
+        private void EventHandler()
+        {
+            UtilTool.SafeExecute(this.mAction);
+        }
+    }
+
+    public class UnityEventBinder<T0> : UnityEventBinderBase
+    {
+        private UnityEvent<T0>      mUnityEvent;
+        private readonly Action<T0> mAction;
+
+        public UnityEventBinder(UnityEventBase rUnityEvent, Action<T0> rAction)
+        {
+            this.mUnityEvent = (UnityEvent<T0>)rUnityEvent;
+            this.mAction     = rAction;
+
+            this.mUnityEvent.AddListener(EventHandler);
+        }
+
+        public override void Dispose()
+        {
+            if (mUnityEvent == null) return;
+
+            mUnityEvent.RemoveListener(EventHandler);
+            mUnityEvent = null;
+        }
+
+        private void EventHandler(T0 rArg0)
+        {
+            UtilTool.SafeExecute(this.mAction, rArg0);
+        }
+    }
+
+    public class UnityEventBinder<T0, T1> : UnityEventBinderBase
+    {
+        private UnityEvent<T0, T1>      mUnityEvent;
+        private readonly Action<T0, T1> mAction;
+
+        public UnityEventBinder(UnityEventBase rUnityEvent, Action<T0, T1> rAction)
+        {
+            this.mUnityEvent = (UnityEvent<T0, T1>)rUnityEvent;
+            this.mAction     = rAction;
+
+            this.mUnityEvent.AddListener(EventHandler);
+        }
+
+        public override void Dispose()
+        {
+            if (mUnityEvent == null) return;
+
+            mUnityEvent.RemoveListener(EventHandler);
+            mUnityEvent = null;
+        }
+
+        private void EventHandler(T0 rArg0, T1 rArg1)
+        {
+            UtilTool.SafeExecute(this.mAction, rArg0, rArg1);
+        }
+    }
+
+    public class UnityEventBinder<T0, T1, T2> : UnityEventBinderBase
+    {
+        private UnityEvent<T0, T1, T2>      mUnityEvent;
+        private readonly Action<T0, T1, T2> mAction;
+
+        public UnityEventBinder(UnityEventBase rUnityEvent, Action<T0, T1, T2> rAction)
+        {
+            this.mUnityEvent = (UnityEvent<T0, T1, T2>)rUnityEvent;
+            this.mAction = rAction;
+
+            this.mUnityEvent.AddListener(EventHandler);
+        }
+
+        public override void Dispose()
+        {
+            if (mUnityEvent == null) return;
+
+            mUnityEvent.RemoveListener(EventHandler);
+            mUnityEvent = null;
+        }
+
+        private void EventHandler(T0 rArg0, T1 rArg1, T2 rArg2)
+        {
+            UtilTool.SafeExecute(this.mAction, rArg0, rArg1, rArg2);
+        }
+    }
+
+    public class UnityEventBinder<T0, T1, T2, T3> : UnityEventBinderBase
+    {
+        private UnityEvent<T0, T1, T2, T3>      mUnityEvent;
+        private readonly Action<T0, T1, T2, T3> mAction;
+
+        public UnityEventBinder(UnityEventBase rUnityEvent, Action<T0, T1, T2, T3> rAction)
+        {
+            this.mUnityEvent = (UnityEvent<T0, T1, T2, T3>)rUnityEvent;
+            this.mAction = rAction;
+
+            this.mUnityEvent.AddListener(EventHandler);
+        }
+
+        public override void Dispose()
+        {
+            if (mUnityEvent == null) return;
+
+            mUnityEvent.RemoveListener(EventHandler);
+            mUnityEvent = null;
+        }
+
+        private void EventHandler(T0 rArg0, T1 rArg1, T2 rArg2, T3 rArg3)
+        {
+            UtilTool.SafeExecute(this.mAction, rArg0, rArg1, rArg2, rArg3);
+        }
+    }
+}
