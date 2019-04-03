27acbcc8eefa8c6700517d749433dd2faea9e17a
winddyhe <hgplan@126.com>
2019-04-03 18:42:37 +0800

    Add TweeningAnimatorInspector.

--- knight-client/Packages/Knight-Lib/Knight.Framework.Tweening/Editor/TweeningAnimatorInspector.cs
+++ knight-client/Packages/Knight-Lib/Knight.Framework.Tweening/Editor/TweeningAnimatorInspector.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace Knight.Framework.Tweening.Editor
@@ -8,5 +10,164 @@ namespace Knight.Framework.Tweening.Editor
     [CustomEditor(typeof(TweeningAnimator), true)]
     public class TweeningAnimatorInspector : UnityEditor.Editor
     {
+        protected TweeningAnimator      mTweeningAnimator;
+        protected SerializedProperty    mActionsProp;
+        protected ReorderableList       mReorderableActionList;
+
+        protected GUIContent            mLoopContent;
+        protected GUIContent            mLoopTypeContent;
+        protected GUIContent            mLoopCountContent;
+        protected GUIContent            mDurationContent;
+        protected GUIContent            mTimeCureveContent;
+
+        private void OnEnable()
+        {
+            this.mLoopContent = new GUIContent("Loop: ");
+            this.mLoopTypeContent = new GUIContent("Type");
+            this.mLoopCountContent = new GUIContent("Count");
+            this.mDurationContent = new GUIContent("Duration: ");
+            this.mTimeCureveContent = new GUIContent("TimeCurve: ");
+
+            this.mTweeningAnimator = this.target as TweeningAnimator;
+            this.mActionsProp = this.serializedObject.FindProperty("Actions");
+            this.BuildReorderableActionList();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Add Tweening Action"))
+            {
+                this.AddNewAction();
+            }
+
+            this.mReorderableActionList.DoLayoutList();
+
+            this.serializedObject.ApplyModifiedProperties();
+        }
+
+        private void BuildReorderableActionList()
+        {
+            this.mReorderableActionList = new ReorderableList(this.serializedObject, this.mActionsProp, true, true, false, true);
+            this.mReorderableActionList.drawHeaderCallback = this.ReorderableActionList_DrawHeader;
+            this.mReorderableActionList.drawElementCallback = this.ReorderableActionList_DrawElement;
+            this.mReorderableActionList.elementHeightCallback = this.ReorderableActionList_ElementHeight;
+        }
+
+        private void ReorderableActionList_DrawHeader(Rect rRect)
+        {
+            EditorGUI.LabelField(rRect, "Tweening Actions: ");
+        }
+
+        private void ReorderableActionList_DrawElement(Rect rRect, int nIndex, bool bIsActive, bool bIsFocused)
+        {
+            var rElementProp = this.mActionsProp.GetArrayElementAtIndex(nIndex);
+            var rIsEnableProp = rElementProp.FindPropertyRelative("IsEnable");
+            var rIsFoldProp = rElementProp.FindPropertyRelative("IsFold");
+            var rTypeProp = rElementProp.FindPropertyRelative("Type");
+            var rIsLoopProp = rElementProp.FindPropertyRelative("IsLoop");
+            var rLoopTypeProp = rElementProp.FindPropertyRelative("LoopType");
+            var rLoopCountProp = rElementProp.FindPropertyRelative("LoopCount");
+            var rDurationProp = rElementProp.FindPropertyRelative("Duration");
+            var rTimeCurveProp = rElementProp.FindPropertyRelative("TimeCurve");
+
+            var rCurRect = rRect;
+            rCurRect.y += 2;
+            using (var space = new EditorGUILayout.HorizontalScope())
+            {
+                rCurRect.x += 10;
+                rCurRect.height = 16;
+                rCurRect.width = 15;
+                var bIsFold = EditorGUI.Foldout(rCurRect, rIsFoldProp.boolValue, nIndex.ToString());
+                rIsFoldProp.boolValue = bIsFold;
+
+                rCurRect.x += 15;
+                rCurRect.width = 15;
+                EditorGUI.PropertyField(rCurRect, rIsEnableProp, GUIContent.none);
+
+                GUI.enabled = rIsEnableProp.boolValue;
+
+                rCurRect.x += 20;
+                rCurRect.width = 90;
+                EditorGUI.PropertyField(rCurRect, rTypeProp, GUIContent.none);
+            }
+
+            if (rIsFoldProp.boolValue)
+            {
+                // 第2行 画Duration
+                rCurRect = rRect;
+                rCurRect.y += 22;
+                rCurRect.x += 25;
+                rCurRect.width = 150;
+                rCurRect.height = 16;
+                EditorGUIUtility.labelWidth = 70f;
+                EditorGUI.PropertyField(rCurRect, rDurationProp, this.mDurationContent);
+
+                // 第3行 画Loop
+                rCurRect = rRect;
+                rCurRect.y += 20 * 2 + 2;
+                rCurRect.x += 25;
+                rCurRect.width = 85;
+                rCurRect.height = 16;
+                EditorGUIUtility.labelWidth = 70;
+                EditorGUI.PropertyField(rCurRect, rIsLoopProp, this.mLoopContent);
+                if (rIsLoopProp.boolValue)
+                {
+                    var rTmpRect = new Rect(rRect.x + 25, rCurRect.y-2, 350, 20);
+                    EditorGUI.DrawRect(rTmpRect, new Color(0.2f, 0.2f, 0.2f, 0.2f));
+
+                    rCurRect.x += 155;
+                    rCurRect.width = 90;
+                    rCurRect.height = 16;
+                    EditorGUIUtility.labelWidth = 35f;
+                    EditorGUI.PropertyField(rCurRect, rLoopTypeProp, this.mLoopTypeContent);
+
+                    rCurRect.x += 100;
+                    rCurRect.width = 90;
+                    rCurRect.height = 16;
+                    EditorGUIUtility.labelWidth = 40f;
+                    EditorGUI.PropertyField(rCurRect, rLoopCountProp, this.mLoopCountContent);
+                }
+
+                // 第4行 画曲线抬头
+                rCurRect = rRect;
+                rCurRect.y += 20 * 3 + 2;
+                rCurRect.x += 25;
+                rCurRect.width = 75;
+                rCurRect.height = 16;
+                EditorGUI.LabelField(rCurRect, this.mTimeCureveContent);
+                // 第5行 画曲线
+                rCurRect = rRect;
+                rCurRect.y += 20 * 4;
+                rCurRect.x += 95;
+                rCurRect.width = 75;
+                rCurRect.height = 60;
+                EditorGUI.PropertyField(rCurRect, rTimeCurveProp, GUIContent.none);
+
+                // 后面的画动画类型参数
+            }
+
+            GUI.enabled = true;
+            this.serializedObject.ApplyModifiedProperties();
+        }
+
+        private float ReorderableActionList_ElementHeight(int nIndex)
+        {
+            var rElementProp = this.mActionsProp.GetArrayElementAtIndex(nIndex);
+            var rIsFoldProp = rElementProp.FindPropertyRelative("IsFold");
+            if (rIsFoldProp.boolValue)
+            {
+                return 144f;
+            }
+            else
+            {
+                return 22f;
+            }
+        }
+
+        private void AddNewAction()
+        {
+            var rLastIndex = this.mActionsProp.arraySize;
+            this.mActionsProp.InsertArrayElementAtIndex(rLastIndex);
+        }
     }
 }
--- knight-client/Packages/Knight-Lib/Knight.Framework.Tweening/Runtime/TweeningAnimator.cs
+++ knight-client/Packages/Knight-Lib/Knight.Framework.Tweening/Runtime/TweeningAnimator.cs
@@ -11,6 +11,7 @@ namespace Knight.Framework.Tweening
 {
     public enum TweeningActionType
     {
+        None,
         Position,
         LocalPosition,
         Rotate,
@@ -22,71 +23,86 @@ namespace Knight.Framework.Tweening
     }
     
     [System.Serializable]
-    public class TweeningAction
+    public class TweeningActionBase
     {
-        [NonSerialized]
-        public Tweener              Tweener;
-        public TweeningActionType   Type;
-        public float                Duration;
     }
 
     [System.Serializable]
-    public class TweeningAction_Position : TweeningAction
+    public class TweeningAction_Position : TweeningActionBase
     {
         public Vector3              Start;
         public Vector3              End;
     }
 
     [System.Serializable]
-    public class TweeningAction_LocalPosition : TweeningAction
+    public class TweeningAction_LocalPosition : TweeningActionBase
     {
         public Vector3              Start;
         public Vector3              End;
     }
 
     [System.Serializable]
-    public class TweeningAction_Rotate : TweeningAction
+    public class TweeningAction_Rotate : TweeningActionBase
     {
         public Vector3              Start;
         public Vector3              End;
     }
 
     [System.Serializable]
-    public class TweeningAction_LocalRotate : TweeningAction
+    public class TweeningAction_LocalRotate : TweeningActionBase
     {
         public Vector3              Start;
         public Vector3              End;
     }
 
     [System.Serializable]
-    public class TweeningAction_LocalScale : TweeningAction
+    public class TweeningAction_LocalScale : TweeningActionBase
     {
         public Vector3              Start;
         public Vector3              End;
     }
 
     [System.Serializable]
-    public class TweeningAction_Color : TweeningAction
+    public class TweeningAction_Color : TweeningActionBase
     {
         public Color                Start;
         public Color                End;
     }
 
     [System.Serializable]
-    public class TweeningAction_CanvasAlpha : TweeningAction
+    public class TweeningAction_CanvasAlpha : TweeningActionBase
     {
         public float                Start;
         public float                End;
     }
 
     [System.Serializable]
-    public class TweeningAction_Delay : TweeningAction
+    public class TweeningAction_Delay : TweeningActionBase
     {
+    }
+
+    [System.Serializable]
+    public class TweeningAction
+    {
+        public bool                 IsEnable;
+        public bool                 IsFold;
+
+        public bool                 IsLoop;
+        public LoopType             LoopType;
+        public int                  LoopCount;
+
+        public float                Duration;
+        public AnimationCurve       TimeCurve;
+
+        public TweeningActionType   Type;
+        public TweeningActionBase   Action;
+
+        [NonSerialized]
+        public Tweener              Tweener;
     }
     
     public class TweeningAnimator : MonoBehaviour
     {
-        [ReorderableObjectList]
         public List<TweeningAction> Actions;
     }
 }
